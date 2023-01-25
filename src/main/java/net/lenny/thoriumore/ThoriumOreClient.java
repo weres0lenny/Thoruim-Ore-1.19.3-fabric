c37912a2dd0f1dfc48feb22359910de25e2daffc
weres0lenny <billie.jean.nasral.v.kuvshin.au.au@gmail.com>
2023-01-26 04:38:55 +0500

    Initial commit

---
+++ src/main/java/net/lenny/thoriumore/ThoriumOreClient.java
@@ -0,0 +1,11 @@
+package net.lenny.thoriumore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThoriumOreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
