c4e77e83bc1fdca3bd360014bd82a1abc265fc7f
weres0lenny <billie.jean.nasral.v.kuvshin.au.au@gmail.com>
2023-01-26 05:15:39 +0500

    commit

--- src/main/java/net/lenny/thoriumore/ThoriumOre.java
+++ src/main/java/net/lenny/thoriumore/ThoriumOre.java
@@ -1,15 +1,26 @@
 package net.lenny.thoriumore;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.lenny.thoriumore.item.ModItems;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static net.lenny.thoriumore.item.ModItems.CITRINE;
+
 //very important comment
 public class ThoriumOre implements ModInitializer {
 	public static final String MOD_ID = "thoriumore";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+
+	}
+
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
